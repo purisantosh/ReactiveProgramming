224e312fa22af73ff806f16b457e91c231c272ec
purisantosh <116016459+purisantosh@users.noreply.github.com>
2026-03-10 21:08:14 -0400

    uncomented mono test code

--- src/test/java/com/reactive/webflux/MonoFluxTest.java
+++ src/test/java/com/reactive/webflux/MonoFluxTest.java
@@ -6,12 +6,12 @@ import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
 
-//    @Test
-//    public void testMono(){
-//        Mono<?> monoString = Mono.just("santosh")
-//                .then(Mono.error(new RuntimeException("Exception occured"))).log();
-//        monoString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
-//    }
+    @Test
+    public void testMono(){
+        Mono<?> monoString = Mono.just("santosh")
+                .then(Mono.error(new RuntimeException("Exception occured"))).log();
+        monoString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
+    }
 
 
     @Test
